624acb304ffe271f830418032f82d71f6283c831
Nooa <nooa.lumilaakso@gmail.com>
2020-03-08 21:17:44 +0200

    Base for styled-components and ExerciseOne styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,41 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Menu } from "./pages/Menu";
 import { ExerciseOne } from "./pages/ExerciseOne";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+// Overwrite browser defaults
+const GlobalStyle = createGlobalStyle`
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+`;
+
+// Color scheme for theme provider
+const mainTheme = {
+	main: "#669D31",
+	secondary: "#0B7A75",
+	accent: "#DB324D",
+	dark: "#394648",
+	light: "#EDDDD4"
+};
 
 const App = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" children={<ExerciseOne />} />
-			</Switch>
-		</Router>
+		<>
+			<GlobalStyle />
+			<Router>
+				<ThemeProvider theme={mainTheme}>
+					<Switch>
+						<Route exact path="/" children={<Menu />} />
+						<Route exact path="/exercise1">
+							<ExerciseOne />
+						</Route>
+					</Switch>
+				</ThemeProvider>
+			</Router>
+		</>
 	);
 };
 
--- front-end/src/pages/ExerciseOne.jsx
+++ front-end/src/pages/ExerciseOne.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const words = [
 	"Robin",
@@ -12,6 +13,41 @@ const words = [
 	"Chaffinch",
 	"Long-tailed tit"
 ];
+
+const Page = styled.div`
+	display: flex;
+	flex-flow: column nowrap;
+	height: 100vh;
+	width: 100vw;
+	background-color: ${props => props.theme.secondary};
+`;
+
+const Container = styled.div`
+	align-items: center;
+	color: white;
+	display: flex;
+	flex-flow: row nowrap;
+	font-family: Arial;
+	font-size: 2.5rem;
+	height: 100%;
+	justify-content: center;
+	text-align: center;
+	width: 100%;
+`;
+
+const Side = styled.div`
+	color: ${props => props.theme.accent};
+	font-size: 1.2em;
+	font-weight: 600;
+	transform: ${props => (props.active ? "scale(1)" : "scale(0)")};
+	transition: transform 0.2s;
+`;
+
+const WordDisplay = styled.h2`
+	display: inline-block;
+	color: ${props => props.theme.light};
+	margin: 1rem;
+`;
 
 // Handle switching to next value, and return updated state
 const nextValue = state => {
@@ -82,13 +118,13 @@ const ExerciseOne = () => {
 	}, [gameState]);
 
 	return (
-		<div>
-			<p>
-				{gameState.side && <span> &gt; </span>}
-				{gameState.value}
-				{!gameState.side && <span> &lt; </span>}
-			</p>
-		</div>
+		<Page>
+			<Container>
+				<Side active={gameState.side}> &gt; </Side>
+				<WordDisplay>{gameState.value} </WordDisplay>
+				<Side active={!gameState.side}> &lt; </Side>
+			</Container>
+		</Page>
 	);
 };
 
